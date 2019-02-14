a778e641260dd279e0081c1daf8fbb0378ee08ba
Dylan Buehler <dylanbuehler1999@gmail.com>
2019-02-14 11:11:45 -0600

    Rock'in Paper Scissors
    Got the game displaying the player choice and AI choice. Also changing a text property to tell the player if they win or not.

--- RPSPrototype/RockPaperScissors/Assets/RPSLogic.cs
+++ RPSPrototype/RockPaperScissors/Assets/RPSLogic.cs
@@ -9,10 +9,19 @@ public class RPSLogic : MonoBehaviour
     public GameObject paperPrefab;
     public GameObject rockPrefab;
     public GameObject scissorsPrefab;
+    public Text messageChange;
 
     // Use this for initialization
-    void Start () {
-		
+    void Start ()
+    {
+        //Find a reference to the message text
+        GameObject message = GameObject.Find("Message");
+
+        //Get the text component from the GameObject
+        messageChange = message.GetComponent<Text>();
+
+        //Set the starting message
+        messageChange.text = "Pick a Fighter!";
 	}
 	
 	// Update is called once per frame
@@ -20,11 +29,28 @@ public class RPSLogic : MonoBehaviour
     {
         GameObject playerChoice = ChooseFighter();
 
-        if (playerChoice != null)//playerChoice.GetComponent("paperPrefab") || playerChoice.GetComponent("rockPrefab") || playerChoice.GetComponent("scissorPrefab"))
+        if (playerChoice != null)
         {
             GameObject aIChoice = AIChoice();
-            Instantiate(playerChoice, new Vector3(-10, 0, 0), Quaternion.identity);
-            Instantiate(aIChoice, new Vector3(10, 0, 0), Quaternion.identity);
+
+            if(playerChoice == scissorsPrefab)
+            {
+                Instantiate(playerChoice, new Vector3(-8, 4, 0), Quaternion.identity);
+            }
+            else if(playerChoice == paperPrefab || playerChoice == rockPrefab)
+            {
+                Instantiate(playerChoice, new Vector3(-10, 0, 0), Quaternion.identity);
+            }//End if / else
+
+            if (aIChoice == scissorsPrefab)
+            {
+                Instantiate(aIChoice, new Vector3(8, 4, 0), Quaternion.identity);
+            }
+            else if(aIChoice == rockPrefab || aIChoice == paperPrefab)
+            {
+                Instantiate(aIChoice, new Vector3(10, 0, 0), Quaternion.identity);
+            }//End if / else
+
             Winner(playerChoice, aIChoice);
             playerChoice = null;
         }//End if
@@ -34,24 +60,52 @@ public class RPSLogic : MonoBehaviour
     public void Winner(GameObject player, GameObject aI)
     {
         //All the ways the player can lose
-        if(player.GetComponent("paperPrefab") && aI.GetComponent("scissorsPrefab"))
+        if(player == paperPrefab)
         {
-            Text message = this.GetComponent<Text>();
-            message.text = "You Lose. Try Again.";
+            if(aI == rockPrefab)
+            {
+                messageChange.text = "You Win; Paper Covers Rock!";
+            }
+            else if(aI == scissorsPrefab)
+            {
+                messageChange.text = "You Lose; Paper Gets Cut By Scissors!";
+            }
+            else
+            {
+                messageChange.text = "It's A Tie; Paper And Paper!";
+            }//End if / if else / else
         }
-        else if(player.GetComponent("rockPrefab") && aI.GetComponent("paperPrefab"))
+        else if(player == rockPrefab)
         {
-            Text message = this.GetComponent<Text>();
-            message.text = "You Lose. Try Again.";
+            if (aI == paperPrefab)
+            {
+                messageChange.text = "You Lose; Rock Gets Covered By Paper!";
+            }
+            else if (aI == scissorsPrefab)
+            {
+                messageChange.text = "You Win; Rock Crushes Scissors!";
+            }
+            else
+            {
+                messageChange.text = "It's A Tie; Rock And Rock!";
+            }//End if / if else / else
         }
-        else if(player.GetComponent("scissorsPrefab") && aI.GetComponent("rockPrefab"))
+        else if(player == scissorsPrefab)
         {
-            Text message = this.GetComponent<Text>();
-            message.text = "You Lose. Try Again.";
-        }
-
-        //All the ways the player can win
-    }
+            if (aI == paperPrefab)
+            {
+                messageChange.text = "You Win; Scissors Cuts Paper!";
+            }
+            else if (aI == rockPrefab)
+            {
+                messageChange.text = "You Lose; Scissors Gets Crushed By Rock!";
+            }
+            else
+            {
+                messageChange.text = "It's A Tie; Scissors And Scissors!";
+            }//End if / if else / else
+        }//End if / else if / else if
+    }//End Winner
 
     public GameObject ChooseFighter()
     {
@@ -83,7 +137,7 @@ public class RPSLogic : MonoBehaviour
     public GameObject AIChoice()
     {
         GameObject aIChoice;
-        int number = Random.Range(1, 3);
+        int number = Random.Range(1, 4);
 
         if(number == 1)
         {
