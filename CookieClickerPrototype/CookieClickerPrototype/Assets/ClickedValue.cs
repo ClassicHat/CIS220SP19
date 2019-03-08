0af4429256ac6e2b538dd15e386c54e1230733c4
Dylan Buehler <dylanbuehler1999@gmail.com>
2019-03-07 21:06:39 -0600

    initial commit of the cookie clicker prototype

---
+++ CookieClickerPrototype/CookieClickerPrototype/Assets/ClickedValue.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickedValue : MonoBehaviour
+{
+    public Text score;
+    public GameObject ClickValue;
+
+    // Use this for initialization
+    void Start ()
+    {
+        //Find a reference to the clickedValue gameobject
+        GameObject scoreGet = GameObject.Find("ClickedValue");
+
+        //Get the text component from the GameObject
+        score = scoreGet.GetComponent<Text>();
+
+        //Set the starting number of points to +1
+        score.text = "+1";
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Destroy(ClickValue, 1);
+    }
+}
